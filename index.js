7bba0aa6db8b20440a817248caff3cb8d25eab2a
BikromRoy1 <bikrom@peoplentech.com.bd>
2024-08-17 11:28:07 +0600

    request data in body

--- index.js
+++ index.js
@@ -8,6 +8,8 @@
 
 // dependencies
 const http = require('http');
+const url = require('url');
+const { StringDecoder } = require('string_decoder');
 
 // app object - module scaffolding
 const app = {};
@@ -27,8 +29,27 @@ app.createServer = () => {
 
 // handle Request response
 app.handleRequest = (req, res) => {
-  // response handler
-  res.end('Hello World');
+  // Request handling function
+  // get the url and parse it
+  const parseUrl = url.parse(req.url, true);
+  const path = parseUrl.path;
+  const trimmedPath = path.replace(/^\/+|\/+$/g, '');
+  const method = req.method.toLowerCase();
+  const queryStringObject = parseUrl.query;
+  const headerObject = req.headers;
+
+  const decoder = new StringDecoder('utf-8');
+  let realData = '';
+  req.on('data', (buffer) => {
+    realData += decoder.write(buffer);
+  });
+
+  req.on('end', () => {
+    realData += decoder.end();
+    console.log(realData);
+    // response handler
+    res.end('Hello World');
+  });
 };
 
 // start the server
